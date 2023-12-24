432114279369f8a27158ce38e3dafbeb2c83dbb9
csnkarthik <karthik.aspx.cs@gmail.com>
2023-12-23 23:22:02 -0800

    module practice done

---
+++ modules-poc/main.tf
@@ -0,0 +1,5 @@
+module "my_resource_group" {
+    source = "./my_resource_group"
+    resource_location = "westus2"
+    resource_group_name = "test-rg"
+}
---
+++ modules-poc/my_resource_group/variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+  # default = "myTFResourceGroup"
+  description = "Resource Group Name"
+}
+
+variable "resource_location" {
+  description = "Location"
+
+  # default = "westus2"
+  # type = string  
+}
